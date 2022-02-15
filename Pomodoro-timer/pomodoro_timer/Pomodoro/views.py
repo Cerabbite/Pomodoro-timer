a6b258fc1c72cda08f6422b5dc5ead32f65bcbee
KevinGiesberts <kevin.giesberts@gmail.com>
2022-02-15 20:01:39 +0100

    Create

--- Pomodoro-timer/pomodoro_timer/Pomodoro/views.py
+++ Pomodoro-timer/pomodoro_timer/Pomodoro/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Hello world!')
+    #return HttpResponse('Hello world!')
+    return render(request, "index.html")
